ded7b28985cc32093924663b7ac6f90173f0ee37
Grzegorz Surdziel <surdziel.grzegorz@gmail.com>
2019-11-21 19:43:32 +0100

    temp

---
+++ src/Problem9.java
@@ -0,0 +1,10 @@
+public class Problem9 extends Solution {
+    public static Object solve(){
+        //A^2 + B^2 = C^2
+        //A+B+C = 1000
+        int target = 1000;
+
+
+        return new Object();
+    }
+}
